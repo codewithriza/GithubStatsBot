16c24fe6858c4a46c8cfd5368c156d7bbb5735f8
Riza <90960705+codewithriza@users.noreply.github.com>
2024-03-14 16:48:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -13,4 +16,4 @@ if __name__ == '__main__':
     for extension in initial_extensions:
         bot.load_extension(extension)
 
-    bot.run('TOKEN')
+    bot.run(os.getenv('TOKEN'))

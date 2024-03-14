47c19f3aae88f11a6884c55462d76fb7ea3d156a
Riza <90960705+codewithriza@users.noreply.github.com>
2024-03-14 16:44:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from discord.ext import commands
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+initial_extensions = [
+    'cogs.user',
+    'cogs.repos'
+]
 
-bot.load_extension('cogs.repos')
-bot.load_extension('cogs.user')
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
 
-bot.run('TOKEN')
+    bot.run('TOKEN')
